86901b45990afbc6479d6fa8393b130675010f5e
OTHSCS107 <OTHSCS107@KATYISD.ORG>
2019-11-04 08:15:32 -0600

    cool

---
+++ main.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+import math
+import csv
+
+def sigmoid(Z):
+    return 1/(1+np.exp(-Z))
+
+
+'''
+def cost:
+    return (T * np.log(Y) + (1 - T) * np.log(1 - Y)).sum()
+
+def gradient_BCE(Y, T):
+    
+    return (T/Y) - (1-T)/(1-Y)
+'''
+def run():
+
+ return 0
+
+
+
+
+
