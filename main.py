4488dc2e7938939cfda397aa51a71d3fc1c9b201
OTHSCS107 <OTHSCS107@KATYISD.ORG>
2019-11-07 08:10:57 -0600

    added data

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import random
 import math
 import csv
 from process_data import process
+
 def sigmoid(Z):
     return 1/(1+np.exp(-Z))
 
@@ -16,11 +17,21 @@ def gradient_BCE(Y, T):
     
     return (T/Y) - (1-T)/(1-Y)
 '''
+# split into 80% train and 20% test
 def run():
+     x, y = process('nba_data2016-2018.csv')
+     D= x.shape[1]
+     M= 20
 
- return 0
+     B= np.random.rand(M)
+     W1= np.random.rand(D,M)
+     B2 = np.random.rand(1)
+     W2 = np.random.rand(M,1)
+
+
+     return 0
 
 
 
-
+run()
 
--- process_data.py
+++ process_data.py
@@ -11,15 +11,13 @@ data_top = df.head()
 #X['Price'] = X['Price'].str.replace('$', '')
 
 Dict = {0: 'ATL', 1: 'BRK',  
-        2:'BOS', 3: 'CHA' , 4: 'CHI' , 5: 'CLE' , 6: 'DAL', 7: 'DEN' , 8: 'DET' , 9: 'GSW', 10: 'HOU', 11: 'IND',12: 'LAC' , 13: 'LAL' , 14: 'MEM' , 15: 'MIA', 
+        2:'BOS', 3: 'CHO' , 4: 'CHI' , 5: 'CLE' , 6: 'DAL', 7: 'DEN' , 8: 'DET' , 9: 'GSW', 10: 'HOU', 11: 'IND',12: 'LAC' , 13: 'LAL' , 14: 'MEM' , 15: 'MIA', 
         16: 'MIL', 17: 'MIN', 18: 'NOP', 19: 'NYK', 
         20: 'OKC', 21: 'ORL',
-        22:'PHI', 23: 'PHX' ,24: 'POR' ,
+        22:'PHI', 23: 'PHO' ,24: 'POR' ,
         25: 'SAC',26: 'SAS',27: 'TOR' ,28: 'UTA',29: 'WAS'} 
     
-# iterating the columns 
-for row in data_top.index: 
-    print(row, end = " ") 
+
 
 def process(filename):
     with open(filename, 'r') as csvfile:
@@ -42,26 +40,32 @@ def process(filename):
        if(y>1 and x!=0):
           ball_data[x][y] = float(ball_data[x][y])
 
-    #print(ball_data)
+    
+    
 
     z = np.delete(ball_data, 0, axis=0)
     z2= np.delete(z, 1, axis=1)
-    print(z2[202])
+    
+    z1= np.array(z, dtype= np.float)
+    z3 = np.array(z2 , dtype= np.float)
+    z3[:,0]= (z3[:,0]-np.mean(z3[:,0]))/np.std(z3[:,0])
+    z3[:,1]= (z3[:,1]-np.mean(z3[:,1]))/np.std(z3[:,1])
+    z3[:,2]= (z3[:,2]-np.mean(z3[:,2]))/np.std(z3[:,2])
 
     true_ball_data = []       
     Y= []
-    for a in range(len(z2)):
-        true_ball_data.append(z2[a])
-        Y.append(z[a][1])
+    for a in range(len(z3)):
+        true_ball_data.append(z3[a])
+        Y.append(z1[a][1])
     
+    true_ball_data= np.array(true_ball_data)
+    Y= np.array(Y)
+
     return true_ball_data, Y
 '''
 X2 = np.zeros((N, D+catOHE))
     X2[:,0:D] = X[:,1:D+1]
 
-    z2[:,0]= (z2[:,0]-np.mean(z2[:,0]))/np.std(z2[:,0])
-    z2[:,1]= (z2[:,1]-np.mean(z2[:,1]))/np.std(z2[:,1])
-    z2[:,2]= (z2[:,2]-np.mean(z2[:,2]))/np.std(z2[:,2])
+    
 
 '''
-process('nba_data2016-2018.csv')
